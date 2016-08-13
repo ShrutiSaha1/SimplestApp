9b7286d22285074ee7ada63a7aeba41e29cd903e
Shruti Saha <shruti.saha21@gmail.com>
2016-08-14 00:25:03 +0530

    My very first try as a beginner :)

---
+++ SimplestWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimplestWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            String t1, t2;
+            t1 = textBox.Text;
+            t2 = textBox1.Text;
+            int a = int.Parse(t1);
+            int b = int.Parse(t2);
+            int c = a + b;
+            textBox2.Text = c.ToString();
+
+
+        }
+    }
+}
